ac65d1a796381cf41a52ffa120f56a84dbe34d3b
Mukul Rawat <mukulrawat7017@gmail.com>
2025-03-13 23:48:58 +0530

    profile view api, edit api

--- src/models/user.js
+++ src/models/user.js
@@ -1,56 +1,64 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-   
   },
   lastName: {
-    type: String
+    type: String,
   },
   emailId: {
     type: String,
-    lowercase:true,
+    lowercase: true,
     required: true,
-   unique: true,
-   trim: true,
-   validate(value){
-    if(!validator.isEmail(value)){
-      throw new Error("Invalid Email" + value);
-    }
-   }
+    unique: true,
+    trim: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email" + value);
+      }
+    },
   },
   password: {
     type: String,
     required: true,
   },
   age: {
-    type: Number
+    type: Number,
   },
   gender: {
     type: String,
-    validate(value){
-      if(!["male","female", "others"].includes(value)){
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)) {
         throw new Error("Gender data is not valid");
       }
-    }
+    },
   },
-  about:{
+  about: {
     type: String,
     maxLength: 200,
     trim: true,
-    default: "Hello, I am a developer"
+    default: "Hello, I am a developer",
   },
-  skills:{
-    type:[String],
+  skills: {
+    type: [String],
   },
- 
-},
-{
-  timestamps: true,
-}
-);
-module.exports= mongoose.model("User", userSchema);
+});
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "DEV@TINDER$134", {
+    expiresIn: "7d",
+  });
+  return token;
+};
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = this.password;
 
-
+  const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash);
+  return isPasswordValid;
+};
+module.exports = mongoose.model("User", userSchema);
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,20 +1,32 @@
+const validator = require("validator");
 
-const validator = require('validator');
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
 
-const validateSignUpData = (req) =>{
-    const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("name is not valid");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password");
+  }
+};
+const validateEditprofileData = (req) => {
+  const allowededitFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "age",
+    "gender",
+    "about",
+    "skills",
+  ];
 
-    if(!firstName || !lastName){
-        throw new Error("name is not valid");
-
-    }
-    else if(!validator.isEmail(emailId)){
-        throw new  Error("email is not valid");
-    }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter a strong password");
-    }
-}
+  const iseditAllowed = Object.keys(req.body).every((field) =>
+    allowededitFields.includes(field)
+  );
+  return iseditAllowed;
+};
 module.exports = {
-    validateSignUpData,
-}
+  validateSignUpData,validateEditprofileData
+};
