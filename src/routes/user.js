d6486acbd4af3e0d5722a4365e5ea7b02892347f
Mukul Rawat <mukulrawat7017@gmail.com>
2025-08-23 23:35:09 +0530

    Implement connection requests and connections API with sender/receiver info

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,25 +3,54 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionsRequest");
 const userRouter = express.Router();
 
+//refactoring the code to make it cleaner
+const SAFE_USER_DATA = "firstName lastName photoUrl gender age about skills";
 //get all the pending connection requests for the logged in user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
-    
     const loggedInUser = req.user;
-    
+
     const connectionRequests = await ConnectionRequestModel.find({
-        toUserId: loggedInUser._id,
-        status: "interested"
-    }).populate("fromUserId","firstName lastName photoUrl gender age about skills");
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", SAFE_USER_DATA);
 
     res.json({
-        message: "Connection requests fetched successfully",
-        data: connectionRequests,
-    })
-
-
+      message: "Connection requests fetched successfully",
+      data: connectionRequests,
+    });
   } catch (err) {
     res.status(400).send("Error fetching connection requests: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionsRequest = await ConnectionRequestModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", SAFE_USER_DATA)
+      .populate("toUserId", SAFE_USER_DATA);
+
+    const data = connectionsRequest.map((row) => {
+      if (row.fromUserId._id.equals(loggedInUser._id)) {
+        return row.toUserId;
+      } else {
+        return row.fromUserId;
+      }
+    });
+
+    res.json({
+      message: "Connections fetched successfully",
+      data,
+    });
+  } catch (err) {
+    res.status(400).send("Error fetching connections: " + err.message);
   }
 });
 
